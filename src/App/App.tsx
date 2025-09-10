92f815ccf0f226bf3ae107cce93da37b921d58d6
Max Kyrchanov <max.kyrchanov@gmail.com>
2025-09-10 16:57:57 +0500

    add basic scss files, add basic reset styles

---
+++ src/App/App.tsx
@@ -0,0 +1,13 @@
+import cn from 'clsx'
+import styles from './App.module.scss'
+
+function App() {
+
+  return (
+    <div className={cn(styles['container'])}>
+      E-commerce - онлайн-магазин бесполезных товаров
+    </div>
+  )
+}
+
+export default App
